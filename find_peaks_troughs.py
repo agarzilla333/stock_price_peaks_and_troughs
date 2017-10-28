7b33253bc4a316df446fdc9b98acf0e867e6b62f
Adrian Garza <adrianmgarza@gmail.com>
2017-10-28 10:33:26 -0400

    cleans up imports

--- find_peaks_troughs.py
+++ find_peaks_troughs.py
@@ -1,7 +1,5 @@
-import argparse
+import argparse, xlrd, xlsxwriter
 from datetime import datetime, date
-import xlrd
-import xlsxwriter
 
 ### example: results_dict = { start_date: [start_date, end_date, start_price, end_price, percent_chg, number_days, index_end_date], ...}
 ### example: data = [date, cur_close_price]
